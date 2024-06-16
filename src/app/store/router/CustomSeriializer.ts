997e2a4c732ff6d4c396cd69de3315523878b343
olexandr <alexx1984@ukr.net>
2024-06-16 20:18:15 +0300

    add storage serializer

--- src/app/store/router/CustomSeriializer.ts
+++ src/app/store/router/CustomSeriializer.ts
@@ -1,25 +1,28 @@
-import {Params, RouterStateSnapshot} from '@angular/router';
-import {RouterStateSerializer} from '@ngrx/router-store';
+import { Params, RouterStateSnapshot } from '@angular/router';
+import { RouterStateSerializer } from '@ngrx/router-store';
 
-export interface RouterStateModel {
-  state: any;
+export interface RouterStateUrl {
   url: string;
   params: Params;
   queryParams: Params;
-
 }
 
+export class CustomSerializer implements RouterStateSerializer<RouterStateUrl> {
+  serialize(routerState: RouterStateSnapshot): RouterStateUrl {
+    let route = routerState.root;
 
-export class CustomSerializer implements RouterStateSerializer<RouterStateModel> {
-    serialize(routerState: RouterStateSnapshot): RouterStateModel {
-     let route = routerState.root;
-      while (route.firstChild) {
-        route = route.firstChild;
-      }
+    while (route.firstChild) {
+      route = route.firstChild;
+    }
 
-      const { url, root: { queryParams } } = routerState;
-      const { params } = route;
+    const {
+      url,
+      root: { queryParams },
+    } = routerState;
+    const { params } = route;
 
-      return { url, params, queryParams };
-   }
+    // Only return an object including the URL, params and query params
+    // instead of the entire snapshot
+    return { url, params, queryParams };
+  }
 }
