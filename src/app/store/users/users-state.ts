9a12fdf074de01b6b39b2f44abc9b983662d7de4
Oleksandr Tukhovskyi <alexx1984@ukr.net>
2025-01-17 15:36:22 +0200

    update users state

---
+++ src/app/store/users/users-state.ts
@@ -0,0 +1,32 @@
+import { User } from '../../models/user';
+import { PaginatedUsersResponse } from '../../models/paginate-users-response';
+
+export const initialPaginationResponse: PaginatedUsersResponse = {
+  current_page: 1,
+  data: [],
+  first_page_url: '',
+  from: 1,
+  last_page: 1,
+  last_page_url: '',
+  next_page_url: null,
+  links: [],
+  path: '',
+  per_page: 0,
+  prev_page_url: null,
+  to: 1,
+  total: 0,
+};
+
+export const initialUser: User = {
+  id: 1,
+  name: 'User 1',
+  email: 'email@ukr.net',
+  password: 'password',
+  password_confirmation: 'password',
+};
+
+export const initialState = {
+  user: initialUser,
+  paginationResponse: initialPaginationResponse,
+  errorMessage: null as string | null,
+};
